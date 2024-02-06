9ccad2f1e1afd21f208b0596ab77b2b6280cb33f
Isikawa Shotaro <zhengtailangs7@gmail.com>
2024-02-06 15:15:11 +0900

    button v-bind

--- hello-vue/src/App.vue
+++ hello-vue/src/App.vue
@@ -2,10 +2,12 @@
 import {ref} from "vue"
 
 const url = ref("https://vuejs.org");
+const isSendButtonDisabled = ref(true);
 </script>
 
 <template>
   <p><a v-bind:href="url" target="_blank">Vue.jsのサイト</a></p>
   <p><a :href="url" target="_blank">Vue.jsのサイト</a></p>
   <p><a v-bind:href="url + 'guide/introduction.html'" target="_blank">Vue.jsガイドのページ</a></p>
+  <p><button type="button" v-bind:disabled="isSendButtonDisabled">送信</button></p>
 </template>

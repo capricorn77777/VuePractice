83f25f514358d3dd7aa5f3491f92baa853109a6f
Isikawa Shotaro <zhengtailangs7@gmail.com>
2024-02-09 14:22:45 +0900

    イベントハンドラ、因数の併用

--- hello-vue/src/App.vue
+++ hello-vue/src/App.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
 import {ref} from "vue"
 
-const pBgColor = ref("white");
-const onPClick = (bgColor: string): void => {
+const pMsg = ref("イベント前ここをクリック");
+const pBgColorEvent = ref("white");
+const onPClickWithEvent = (bgColor: string, event: MouseEvent): void => {
   pBgColor.value = bgColor;
+  pMsg.value = event.timeStamp.toString();
 };
 </script>
 
 <template>
    <section>
-      <p v-on:click="onPClick('red')" v-bind:style="{backgroundColor: pBgColor}">
+      <p v-on:click="onPClickWithEvent('green', $event)" v-bind:style="{backgroundColor: pBgColorEvent}">
       ここをクリックすると背景色が変わります
       </p>
    </section>

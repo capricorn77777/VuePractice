52f7da49e13dfd8ee39c14e0452fd428039cbb54
Isikawa Shotaro <zhengtailangs7@gmail.com>
2024-02-10 21:35:50 +0900

    クリックイベント

--- hello-vue/src/App.vue
+++ hello-vue/src/App.vue
@@ -1,15 +1,21 @@
 <script setup lang="ts">
 import {ref} from "vue"
 
-const msg = ref("未送信");
-const onFormSubmit = (): void => {
-  msg.value = "送信されました";
+const msg = ref("まだです");
+const onEntryKey = (): void => {
+  msg.value = "エンターキーが押下されました";
+};
+const onRightButtonClick = (): void => {
+  msg.value = "ボタンが右クリックされました";
+};
+const onShiftClick = (): void => {
+  msg.value = "シフトを押しながらクリックされました"
 };
 </script>
 
 <template>
    <section>
-      <form action="#" v-on:submit.prevent="onFormSubmit">
+      <form action="#" v-on:submit="onFormSubmit">
       <input type="text" required>
       <button type="submit">送信</button>
     </form>

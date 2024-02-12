da647703a97f6fbdccea9bccabb5cafca8d5549e
Isikawa Shotaro <zhengtailangs7@gmail.com>
2024-02-12 16:45:29 +0900

    双方向データバインディング script

--- hello-vue/src/App.vue
+++ hello-vue/src/App.vue
@@ -4,14 +4,15 @@ import {ref} from "vue"
 const inputTextarea = ref("テキストエリアへの入力文字");
 const memberType = ref(1);
 const memberTypeSelect = ref(1);
-const onInputName = (event: Event): void => {
-  const element = event.target as HTMLInputElement;
-  inputNameOn.value = element.value;
-} 
+const isAgreed = ref(false);
+const isAgreed01 = ref(0);
+const selectedOS = ref([]);
+const selectedOSSelect = ref([]);
 
 </script>
 
 <template>
+  <Ap
   <section>
       <input type="text" v-bind:value="inputNameBind"><br>
   </section>
